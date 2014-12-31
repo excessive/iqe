31365b230c2516b2cfacc19bfe3218c3f7660610
Landon Manning <lmanning17@gmail.com>
2014-12-30 19:40:56 -0500

    Formatting

--- iqe.lua
+++ iqe.lua
@@ -26,20 +26,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ]]--
 
-local path = ... .. "."
-local loader = {
-	_LICENSE = "Lua IQE Loader is distributed under the terms of the MIT license. See LICENSE.md.",
-	_URL = "https://github.com/karai17/Lua-IQE-Loader",
-	_VERSION = "0.2.4",
+local path     = ... .. "."
+local cpml     = require "libs.cpml"
+local loader   = {
+	_LICENSE     = "Lua IQE Loader is distributed under the terms of the MIT license. See LICENSE.md.",
+	_URL         = "https://github.com/karai17/Lua-IQE-Loader",
+	_VERSION     = "0.2.5",
 	_DESCRIPTION = "Load an IQE 3D model (and optional MTL material) into Lua.",
 }
-local IQE = {}
-
-local cpml = require "libs.cpml"
-
--- global resource registries
-local models = {}
-local textures = {}
+local IQE      = {}
+local models   = {} -- global cache
+local textures = {} -- global cache
 
 
 --[[ Helper Functions ]]--
@@ -123,16 +120,14 @@ function loader.load(file)
 	local get_lines
 
 	if love then
-		local filetext = love.filesystem.read(file)
-		get_lines = function(file) return filetext:gmatch("[^\r\n]+") end
-
 		textures.blank = love.graphics.newImage(love.image.newImageData(1, 1))
+		local filetext = love.filesystem.read(file)
+		get_lines      = function(file) return filetext:gmatch("[^\r\n]+") end
 	else
-		get_lines = io.lines
+		get_lines      = io.lines
 	end
 
 	local lines = {}
-
 	for line in get_lines(file) do
 		if line == "# Inter-Quake Export" or line[1] ~= "#" then
 			line = string.gsub(line, "\t", "")
@@ -141,8 +136,7 @@ function loader.load(file)
 	end
 
 	if lines[1] == "# Inter-Quake Export" then
-		local model = {}
-		model = setmetatable(model, {__index = IQE})
+		local model = setmetatable({}, {__index = IQE})
 		model:init(lines, file)
 
 		return model
@@ -152,19 +146,18 @@ end
 --[[ Inter-Quake Export ]]--
 
 function IQE:init(lines, file)
-	self.lines = lines
-	self.current_mesh = false
-	self.current_material = false
-	self.current_joint = false
-	self.current_animation = false
-	self.current_frame = false
+	self.lines               = lines
+	self.current_mesh        = false
+	self.current_material    = false
+	self.current_joint       = false
+	self.current_animation   = false
+	self.current_frame       = false
 	self.current_vertexarray = false
-	self.paused = false
-	self.data = {}
-	self.materials = {}
-	self.vertex_buffer = {}
-	self.stats = {}
-
+	self.paused              = false
+	self.data                = {}
+	self.materials           = {}
+	self.vertex_buffer       = {}
+	self.stats               = {}
 	self:parse()
 
 	if love then
@@ -180,7 +173,7 @@ end
 function IQE:parse(lines)
 	self.lines = self.lines or lines
 	for _, line in ipairs(self.lines) do
-		local l = string_split(line, " ")
+		local l   = string_split(line, " ")
 		local cmd = l[1]
 		table.remove(l, 1)
 
@@ -189,14 +182,14 @@ function IQE:parse(lines)
 		end
 	end
 
-	self.lines = nil
-	self.current_mesh = nil
-	self.current_material = nil
-	self.current_joint = nil
-	self.current_animation = nil
-	self.current_frame = nil
+	self.lines               = nil
+	self.current_mesh        = nil
+	self.current_material    = nil
+	self.current_joint       = nil
+	self.current_animation   = nil
+	self.current_frame       = nil
 	self.current_vertexarray = nil
-	self.current_mtl = nil
+	self.current_mtl         = nil
 end
 
 function IQE:load_texture(file, filter)
@@ -216,13 +209,12 @@ function IQE:load_material(file)
 
 	if love then
 		local filetext = love.filesystem.read(file)
-		get_lines = function(file) return filetext:gmatch("[^\r\n]+") end
+		get_lines      = function(file) return filetext:gmatch("[^\r\n]+") end
 	else
-		get_lines = io.lines
+		get_lines      = io.lines
 	end
 
 	local lines = {}
-
 	for line in get_lines(file) do
 		if line[1] ~= "#" then
 			line = string.gsub(line, "\t", "")
@@ -250,25 +242,26 @@ end
 --[[ Meshes ]]--
 
 function IQE:mesh(line)
-	line = merge_quoted(line)
-	self.current_mesh = {}
+	line                   = merge_quoted(line)
+	self.current_mesh      = {}
 	self.current_mesh.name = line[1]
 end
 
 function IQE:material(line)
 	line = merge_quoted(line)
 
-	self.data.material = self.data.material or {}
+	self.data.material          = self.data.material          or {}
 	self.data.material[line[1]] = self.data.material[line[1]] or {}
-	table.insert(self.data.material[line[1]], self.current_mesh)
-	self.current_material = self.data.material[line[1]]
+	self.current_material       = self.data.material[line[1]]
+	table.insert(self.current_material, self.current_mesh)
 end
 
 --[[ Vertex Attributes ]]--
 
 function IQE:vp(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.vp = mesh.vp or {}
+	mesh.vp    = mesh.vp or {}
+
 	local vp = {}
 	for _, v in ipairs(line) do
 		table.insert(vp, tonumber(v))
@@ -276,53 +269,63 @@ function IQE:vp(line)
 	if #vp == 3 then
 		table.insert(vp, 1)
 	end
+
 	table.insert(mesh.vp, vp)
 end
 
 function IQE:vt(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.vt = mesh.vt or {}
+	mesh.vt    = mesh.vt or {}
+
 	local vt = {}
 	for _, v in ipairs(line) do
 		table.insert(vt, tonumber(v))
 	end
+
 	table.insert(mesh.vt, vt)
 end
 
 function IQE:vn(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.vn = mesh.vn or {}
+	mesh.vn    = mesh.vn or {}
+
 	local vn = {}
 	for _, v in ipairs(line) do
 		table.insert(vn, tonumber(v))
 	end
+
 	table.insert(mesh.vn, vn)
 end
 
 function IQE:vx(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.vx = mesh.vx or {}
+	mesh.vx    = mesh.vx or {}
+
 	local vx = {}
 	for _, v in ipairs(line) do
 		table.insert(vx, tonumber(v))
 	end
+
 	table.insert(mesh.vx, vx)
 end
 
 function IQE:vb(line)
-	local mesh = self.current_material[#self.current_material]
+	local mesh  = self.current_material[#self.current_material]
 	self.rigged = true
-	mesh.vb = mesh.vb or {}
+	mesh.vb     = mesh.vb or {}
+
 	local vb = {}
 	for _, v in ipairs(line) do
 		table.insert(vb, tonumber(v))
 	end
+
 	table.insert(mesh.vb, vb)
 end
 
 function IQE:vc(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.vc = mesh.vc or {}
+	mesh.vc    = mesh.vc or {}
+
 	local vc = {}
 	for _, v in ipairs(line) do
 		table.insert(vc, tonumber(v))
@@ -330,17 +333,20 @@ function IQE:vc(line)
 	if #vc == 3 then
 		table.insert(vc, 1)
 	end
+
 	table.insert(mesh.vc, vc)
 end
 
 function IQE:v0(line, cmd)
-	cmd = cmd or "v0"
+	cmd        = cmd or "v0"
 	local mesh = self.current_material[#self.current_material]
-	mesh[cmd] = mesh[cmd] or {}
+	mesh[cmd]  = mesh[cmd] or {}
+
 	local v0 = {}
 	for _, v in ipairs(line) do
 		table.insert(v0, tonumber(v))
 	end
+
 	table.insert(mesh[cmd], v0)
 end
 
@@ -383,14 +389,16 @@ end
 --[[ Vertex Arrays ]]--
 
 function IQE:vertexarray(line)
-	line = merge_quoted(line)
+	line                  = merge_quoted(line)
 	self.data.vertexarray = self.data.vertexarray or {}
-	local va = {}
-	va.type = line[1]
+
+	local va     = {}
+	va.type      = line[1]
 	va.component = line[2]
-	va.size = tonumber(line[3])
-	va.name = line[4] or line[1]
+	va.size      = tonumber(line[3])
+	va.name      = line[4] or line[1]
 	table.insert(self.data.vertexarray, va)
+
 	self.current_vertexarray = self.data.vertexarray[#self.data.vertexarray]
 end
 
@@ -398,29 +406,33 @@ end
 
 function IQE:fa(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.fa = mesh.fa or {}
+	mesh.fa    = mesh.fa or {}
+
 	local fa = {}
 	for k, v in ipairs(line) do
 		table.insert(fa, tonumber(v))
 	end
+
 	table.insert(mesh.fa, fa)
 end
 
 function IQE:fm(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.fm = mesh.fm or {}
+	mesh.fm    = mesh.fm or {}
+
 	local fm = {}
 	for k, v in ipairs(line) do
 		table.insert(fm, tonumber(v))
 	end
+
 	table.insert(mesh.fm, fm)
 end
 
 --[[ Smoothing ]]--
 
 function IQE:smoothuv(line)
-	local mesh = self.current_material[#self.current_material]
-	local n = tonumber(line[1])
+	local mesh    = self.current_material[#self.current_material]
+	local n       = tonumber(line[1])
 	mesh.smoothuv = false
 
 	if n > 0 then
@@ -429,8 +441,8 @@ function IQE:smoothuv(line)
 end
 
 function IQE:smoothgroup(line)
-	local mesh = self.current_material[#self.current_material]
-	local n = tonumber(line[1])
+	local mesh       = self.current_material[#self.current_material]
+	local n          = tonumber(line[1])
 	mesh.smoothgroup = -1
 
 	if n then
@@ -439,8 +451,8 @@ function IQE:smoothgroup(line)
 end
 
 function IQE:smoothangle(line)
-	local mesh = self.current_material[#self.current_material]
-	local angle = tonumber(line[1])
+	local mesh       = self.current_material[#self.current_material]
+	local angle      = tonumber(line[1])
 	mesh.smoothangle = 180
 
 	if angle then
@@ -450,18 +462,20 @@ end
 
 function IQE:fs(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.fs = mesh.fs or {}
+	mesh.fs    = mesh.fs or {}
+
 	local fs = {}
 	for k, v in ipairs(line) do
 		table.insert(fs, tonumber(v))
 	end
+
 	table.insert(mesh.fs, fs)
 end
 
 function IQE:vs(line)
 	local mesh = self.current_material[#self.current_material]
-	mesh.vs = mesh.vs or {}
-	local vs = tonumber(line[1])
+	mesh.vs    = mesh.vs or {}
+	local vs   = tonumber(line[1])
 	table.insert(mesh.vs, vs)
 end
 
@@ -484,10 +498,10 @@ function IQE:pq(line)
 
 	local joint
 	if not self.current_animation then
-		joint = self.current_joint
+		joint    = self.current_joint
 		joint.pq = pq
 	else
-		joint = self.current_frame
+		joint    = self.current_frame
 		joint.pq = joint.pq or {}
 		table.insert(joint.pq, pq)
 	end
@@ -506,10 +520,10 @@ function IQE:pm(line)
 
 	local joint
 	if not self.current_animation then
-		joint = self.current_joint
+		joint    = self.current_joint
 		joint.pm = pm
 	else
-		joint = self.current_frame
+		joint    = self.current_frame
 		joint.pm = joint.pm or {}
 		table.insert(joint.pm, pm)
 	end
@@ -528,10 +542,10 @@ function IQE:pa(line)
 
 	local joint
 	if not self.current_animation then
-		joint = self.current_joint
+		joint    = self.current_joint
 		joint.pa = pa
 	else
-		joint = self.current_frame
+		joint    = self.current_frame
 		joint.pa = joint.pa or {}
 		table.insert(joint.pa, pa)
 	end
@@ -540,11 +554,11 @@ end
 --[[ Skeleton ]]--
 
 function IQE:joint(line)
-	line = merge_quoted(line)
+	line            = merge_quoted(line)
 	self.data.joint = self.data.joint or {}
-	local joint = {}
-	joint.name = line[1]
-	joint.parent = tonumber(line[2]) + 1 -- fix offset
+	local joint     = {}
+	joint.name      = line[1]
+	joint.parent    = tonumber(line[2]) + 1 -- fix offset
 	table.insert(self.data.joint, joint)
 
 	self.current_joint = joint
@@ -553,61 +567,64 @@ end
 --[[ Animations ]]--
 
 function IQE:animation(line)
-	line = merge_quoted(line)
-	self.data.animation = self.data.animation or {}
-	local name = line[1] or tostring(math.random(0, 99999))
+	line                      = merge_quoted(line)
+	self.data.animation       = self.data.animation or {}
+	local name                = line[1]             or tostring(math.random(0, 99999))
 	self.data.animation[name] = {}
+
 	self.current_animation = self.data.animation[name]
-	self.current_frame = false
+	self.current_frame     = false
 end
 
 function IQE:loop(line)
 	local animation = self.current_animation
-	animation.loop = true
+	animation.loop  = true
 end
 
 function IQE:framerate(line)
-	local animation = self.current_animation
+	local animation     = self.current_animation
 	animation.framerate = tonumber(line[1])
 end
 
 function IQE:frame(line)
-	local animation = self.current_animation
-	animation.frame = animation.frame or {}
+	local animation    = self.current_animation
+	animation.frame    = animation.frame or {}
 	table.insert(animation.frame, {})
+
 	self.current_frame = animation.frame[#animation.frame]
 end
 
 --[[ Wavefront Material File ]]--
 
 function IQE:newmtl(line)
-	line = merge_quoted(line)
+	line                    = merge_quoted(line)
 	self.materials[line[1]] = self.materials[line[1]] or {}
+
 	self.current_mtl = self.materials[line[1]]
 end
 
 function IQE:Ns(line)
-	self.current_mtl.ns = tonumber(line[1])
+	self.current_mtl.ns    = tonumber(line[1])
 end
 
 function IQE:Ka(line)
-	self.current_mtl.ka = { tonumber(line[1]), tonumber(line[2]), tonumber(line[3]) }
+	self.current_mtl.ka    = { tonumber(line[1]), tonumber(line[2]), tonumber(line[3]) }
 end
 
 function IQE:Kd(line)
-	self.current_mtl.kd = { tonumber(line[1]), tonumber(line[2]), tonumber(line[3]) }
+	self.current_mtl.kd    = { tonumber(line[1]), tonumber(line[2]), tonumber(line[3]) }
 end
 
 function IQE:Ks(line)
-	self.current_mtl.ks = { tonumber(line[1]), tonumber(line[2]), tonumber(line[3]) }
+	self.current_mtl.ks    = { tonumber(line[1]), tonumber(line[2]), tonumber(line[3]) }
 end
 
 function IQE:Ni(line)
-	self.current_mtl.ni = tonumber(line[1])
+	self.current_mtl.ni    = tonumber(line[1])
 end
 
 function IQE:d(line)
-	self.current_mtl.d = tonumber(line[1])
+	self.current_mtl.d     = tonumber(line[1])
 end
 
 function IQE:illum(line)
@@ -639,7 +656,7 @@ function IQE:buffer()
 				table.insert(layout, 4)
 			end
 
-			local data = {}
+			local data   = {}
 			local bounds = { min = {}, max = {} }
 			for i=1, #mesh.vp do
 				-- all meshes should have these things...
@@ -693,7 +710,7 @@ function IQE:buffer()
 				table.insert(tris, v[3] + 1)
 			end
 
-			stats.vertices = (stats.vertices or 0) + #mesh.vt
+			stats.vertices  = (stats.vertices  or 0) + #mesh.vt
 			stats.triangles = (stats.triangles or 0) + #mesh.fm
 
 			local m = love.graphics.newMesh(0, nil, "triangles")
@@ -713,11 +730,11 @@ function IQE:buffer()
 
 			-- NOTE: We *HAVE* to use VertexPosition for LOVE to play ball here. Annoying.
 			m:setVertexAttribute("VertexPosition", buffer, 1)
-			m:setVertexAttribute("v_normal", buffer, 2)
-			m:setVertexAttribute("v_coord", buffer, 3)
+			m:setVertexAttribute("v_normal",       buffer, 2)
+			m:setVertexAttribute("v_coord",        buffer, 3)
 			if self.rigged then
-				m:setVertexAttribute("v_bone", buffer, 4)
-				m:setVertexAttribute("v_weight", buffer, 5)
+				m:setVertexAttribute("v_bone",     buffer, 4)
+				m:setVertexAttribute("v_weight",   buffer, 5)
 			end
 			m:setVertexMap(tris)
 		end
@@ -729,32 +746,27 @@ function IQE:buffer()
 	if not self.rigged then return end
 
 	local function calc_bone_matrix(pos, rot, scale)
-		local out = cpml.mat4()
+		return cpml.mat4()
 			:translate(pos)
 			:rotate(rot)
 			:scale(scale)
-		return out
 	end
 
-	--self.base = {}
 	self.inverse_base = {}
 
 	for i, joint in ipairs(self.data.joint) do
 		local pose = joint.pq
-
-		local m = calc_bone_matrix(
+		local m    = calc_bone_matrix(
 			cpml.vec3(pose[1], pose[2], pose[3]),
 			cpml.quat(pose[7], pose[4], pose[5], pose[6]),
 			cpml.vec3(pose[8], pose[9], pose[10])
 		)
-		local inv = m:invert()
+		local inv  = m:invert()
 
 		if joint.parent > 0 then
 			assert(joint.parent < i)
-			--self.base[i] = m * self.base[joint.parent]
 			self.inverse_base[i] = self.inverse_base[joint.parent] * inv
 		else
-			--self.base[i] = m
 			self.inverse_base[i] = inv
 		end
 	end
@@ -769,12 +781,11 @@ function IQE:buffer()
 
 		for f, frame in ipairs(animation.frame) do
 			self.animation_buffer[a][f] = {}
-			local transform = {}
+			local transform             = {}
 
 			for p, pq in ipairs(frame.pq) do
-				local joint = self.data.joint[p]
-
-				local m = calc_bone_matrix(
+				local joint  = self.data.joint[p]
+				local m      = calc_bone_matrix(
 					cpml.vec3(pq[1], pq[2], pq[3]),
 					cpml.quat(pq[7], pq[4], pq[5], pq[6]),
 					cpml.vec3(pq[8], pq[9], pq[10])
@@ -784,10 +795,10 @@ function IQE:buffer()
 				if joint.parent > 0 then
 					assert(joint.parent < p)
 					transform[p] = m * transform[joint.parent]
-					render = self.inverse_base[p] * transform[p]
+					render       = self.inverse_base[p] * transform[p]
 				else
 					transform[p] = m
-					render = self.inverse_base[p] * m
+					render       = self.inverse_base[p] * m
 				end
 
 				table.insert(self.animation_buffer[a][f], render:to_vec4s())
@@ -799,15 +810,15 @@ end
 function IQE:dump()
 	local function create_dump(file, t)
 		local buffer = ""
-		local lines = 0
+		local lines  = 0
 		local function dump_r(t, level)
 			level = level or 1
 			for k,v in pairs(t) do
 				buffer = buffer .. rpad(" ", 12 * level) .. rpad(tostring(k), 3)  .. " " .. tostring(v) .. "\n"
-				lines = lines + 1
+				lines  = lines + 1
 				if lines >= 2048 then
 					love.filesystem.append(file, buffer)
-					lines = 0
+					lines  = 0
 					buffer = ""
 				end
 				if type(v) == "table" then
